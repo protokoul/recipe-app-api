118203a323a287def348abb15f8b6083df68ef93
anubhav <anubhavkoul01@gmail.com>
2023-04-21 22:01:14 +0530

    Recovered delete files from errors branch.

--- app/app/settings.py
+++ app/app/settings.py
@@ -33,12 +33,17 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'core',
+    'user',
+    'recipe',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +135,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,5 @@
 """Database models."""
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
