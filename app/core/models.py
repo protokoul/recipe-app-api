5af4986b70eeb3bcdfa648d65ead60f8bd60f6b3
anubhav <anubhavkoul01@gmail.com>
2023-09-08 20:56:17 +0530

    Linting fixes

--- app/core/models.py
+++ app/core/models.py
@@ -69,11 +69,11 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 
 class Ingredient(models.Model):
     """Ingredient for recipes."""
-    name =models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
